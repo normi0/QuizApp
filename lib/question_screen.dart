5ddeda56a1b6886da69cb2c1be344fa51993f26f
Achi seif <seif23achi@gmail.com>
2023-07-26 13:23:33 +0200

    learning chaining in methodes

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -14,50 +14,56 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   Widget build(BuildContext context) {
     final currentquestion = questions[0];
     return Center(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        /* OutlinedButton.icon(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.white,
+        child: Container(
+      margin: const EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          /* OutlinedButton.icon(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            label: const Text(
+              'Back To Home Screen',
+              style: TextStyle(
+                  fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
           ),
-          label: const Text(
-            'Back To Home Screen',
+          const SizedBox(
+            height: 28,
+          ),
+          const Text(
+            'Questions Screen',
             style: TextStyle(
-                fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white),
+                fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
+          ), */
+          Text(
+            currentquestion.text,
+            style: const TextStyle(
+                fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),
+            textAlign: TextAlign.center,
           ),
-        ),
-        const SizedBox(
-          height: 28,
-        ),
-        const Text(
-          'Questions Screen',
-          style: TextStyle(
-              fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
-        ), */
-        Text(
-          currentquestion.text,
-          style: const TextStyle(
-              fontSize: 17, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        //map is just creationg a new object in this case list with new item based on old ones
-        ...currentquestion.answers.map(
-          (item) {
-            return AnswerButton(answerText: item, onTap: () {});
-          },
-        ),
-        AnswerButton(answerText: currentquestion.answers[0], onTap: () {}),
-        AnswerButton(answerText: currentquestion.answers[1], onTap: () {}),
-        AnswerButton(answerText: currentquestion.answers[2], onTap: () {}),
-        AnswerButton(answerText: currentquestion.answers[3], onTap: () {}),
-/*         AnswerButton(answerText: currentquestion.answers[4], onTap: () {}),
- */
-      ],
+          const SizedBox(
+            height: 30,
+          ),
+          //map is just creationg a new object in this case list with new item based on old ones
+          ...currentquestion.answers.map(
+            (item) {
+              return AnswerButton(answerText: item, onTap: () {});
+            },
+          ),
+
+          /* AnswerButton(answerText: currentquestion.answers[0], onTap: () {}),
+          AnswerButton(answerText: currentquestion.answers[1], onTap: () {}),
+          AnswerButton(answerText: currentquestion.answers[2], onTap: () {}),
+          AnswerButton(answerText: currentquestion.answers[3], onTap: () {}),
+          AnswerButton(answerText: currentquestion.answers[4], onTap: () {}),
+           */
+        ],
+      ),
     ));
   }
 }
